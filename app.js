18a8be5f6e1ccb144a6f238b51ef1a33b85d64d5
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-01-29 10:06:45 +0600

    video22-23-24

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import { config } from 'dotenv'
 import express from 'express'
+import ErrorMiddleware from './middlewares/Error.js'
 config({
     path:"./config/config.env"
 })
 const app=express()
+
+//using middlewares
+app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+)
 
 //Importing and using Routes
 import courses from './routes/courseRouters.js'
@@ -12,3 +21,4 @@ app.use("/api/v1",courses)
 import users from "./routes/usersRouters.js"
 app.use("/api/v1",users)
 export default app
+app.use(ErrorMiddleware)
--- routes/courseRouters.js
+++ routes/courseRouters.js
@@ -1,5 +1,8 @@
 import express from 'express'
-import { getAllCourses } from '../controllers/courseControllers.js'
+import { createCourse, getAllCourses } from '../controllers/courseControllers.js'
 const router=express.Router()
+//get all courses without lectures
 router.route("/courses").get(getAllCourses)
+//create new course -only admin
+router.route("/createcourse").post(createCourse)
 export default router
